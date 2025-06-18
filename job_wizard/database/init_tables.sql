c2f07797b639cfbf348479d080f0d42af26cf29b
segoldin <seg@goldin-rudahl.com>
2025-06-18 16:36:51 +0700

    script to initialize the DB

---
+++ job_wizard/database/init_tables.sql
@@ -0,0 +1,51 @@
+-- Script for initializing the JobWizard database
+-- 
+-- Created by Sally Goldin, 18 June 2025
+--
+
+-- Look up table provides text for education level 
+CREATE TABLE IF NOT EXISTS lu_education (
+	value int,
+	title VARCHAR(24)
+);
+
+-- Populate this lookup table
+INSERT INTO lu_education (value,title) values (0,'Unknown');
+INSERT INTO lu_education (value,title) values (1,'High school diploma');
+INSERT INTO lu_education (value,title) values (2,'Bachelors degree');
+INSERT INTO lu_education (value,title) values (3,'Masters degree');
+INSERT INTO lu_education (value,title) values (4,'Doctoral degree');
+
+-- Registered users
+CREATE TABLE IF NOT EXISTS user (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_email varchar(32),  -- user email, which is the identifier for a user	
+	first_name varchar(32),  
+	last_name  varchar(32),  
+	phone varchar(16),
+	max_education integer,
+	created DATETIME         -- always a good idea to save a time stamp   
+);
+
+-- Job descriptions
+
+CREATE TABLE IF NOT EXISTS job (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	created_by varchar(32),  -- email of creating user (employer)	
+	title varchar(64),  
+	description varchar(1024),  
+	min_education integer,
+	min_years_experience integer,
+	salary integer,
+	hired_person varchar(32) default '',    -- email of person hired
+	is_open integer default 1,   -- 1 if still open, 0 if filled
+	created DATETIME             -- always a good idea to save a time stamp   
+);
+
+CREATE TABLE IF NOT EXISTS job_applications (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	job_id char(6),          -- job ID with leading zeros
+	user_email varchar(32),  -- user who has applied
+	apply_time DATETIME
+);
+
