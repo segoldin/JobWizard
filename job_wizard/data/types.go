5f12f96873e9c8e1bffed122d12e21b7d5c9e93e
segoldin <seg@goldin-rudahl.com>
2025-07-03 17:49:08 +0700

    new structure for job application

--- job_wizard/data/types.go
+++ job_wizard/data/types.go
@@ -2,6 +2,7 @@ package data
 // Structure types used for passing information around
 // Created by Sally Goldin 23 June 2025
 
+// Used for registering a new user
 type User_info struct {
     Email           string   `json:"email"`
     First           string   `json:"first"`
@@ -10,6 +11,7 @@ type User_info struct {
     Education       int      `json:"education"`
 }
 
+// Used both for input and output - to create jobs as well as to return job detail
 type Job_info struct {
     Job_id          string    `json:"job_id"`
     Creator         string    `json:"creator"`
@@ -22,6 +24,7 @@ type Job_info struct {
     Date_posted     string    `json:"date_posted"`
 }
 
+// Used to return information from a job search
 type Job_summary struct {
     Job_id          string    `json:"job_id"`
     Title           string    `json:"title"`
@@ -29,6 +32,7 @@ type Job_summary struct {
     Date_posted     string    `json:"date_posted"`    
 }
 
+// Used to search for jobs
 type Search_criteria struct {
     User_email       string         
     Posted           string 
@@ -36,4 +40,10 @@ type Search_criteria struct {
     Education        int
     Salary           int
     Keyword          string
+}
+
+// Used to apply for a job
+type Submission struct {
+    Email           string   `json:"email"`
+    Job_id          string   `json:"job_id"`   
 }
