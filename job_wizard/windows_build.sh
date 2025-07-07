3bd7c348a6540c7b58a84593419ce7be06597059
segoldin <seg@goldin-rudahl.com>
2025-07-07 18:18:26 +0700

    Build windows EXE for tester

---
+++ job_wizard/windows_build.sh
@@ -0,0 +1,8 @@
+mkdir windows
+mkdir windows/database
+env GOOS=windows GOARCH=amd64 go build -o windows/job_wizard.exe -a .
+cp -p .env_jobwizard windows
+cp -pr database/jobwizard_db windows/database
+zip -r JobWizard.zip windows/*.* windows/.env* windows/database/*
+
+
