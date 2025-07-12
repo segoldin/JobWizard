bcf989e2136f0c693204a040ab170692ab204446
segoldin <seg@goldin-rudahl.com>
2025-07-12 17:50:36 +0700

    Add CGO_ENABLED... but does not work

--- job_wizard/windows_build.sh
+++ job_wizard/windows_build.sh
@@ -1,6 +1,6 @@
 mkdir windows
 mkdir windows/database
-env GOOS=windows GOARCH=amd64 go build -o windows/job_wizard.exe -a .
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o windows/job_wizard.exe -a .
 cp -p .env_jobwizard windows
 cp -pr database/jobwizard_db windows/database
 zip -r JobWizard.zip windows/*.* windows/.env* windows/database/*
